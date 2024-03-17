03ead5d0b7bf9deae2bb29c8a9f37067f64ec510
Lewis M <2864939m@student.gla.ac.uk>
2024-03-17 19:07:28 +0000

    Allowed hosts added

--- practical_test_proj/practical_test_proj/settings.py
+++ practical_test_proj/practical_test_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mu%8_vu45464l0(5bn@5$i=!fewix*wh*^qn)fw89qtb#&930f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mcgl.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
