91b1a28efb5a650381b5a3e4da67dd62fc2435cb
Lewis M <2864939m@student.gla.ac.uk>
2024-03-20 16:15:34 +0000

    Models + admin config

--- practical_test_proj/student_pets/models.py
+++ practical_test_proj/student_pets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    forename = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    catNum = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.forename
+
+
+class Cat(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
